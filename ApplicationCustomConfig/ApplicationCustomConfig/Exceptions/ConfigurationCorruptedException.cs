c559b5239e3a1df5e0ed1fc258ac543d1fefec03
craftersmine <rodion.morozov.01@gmail.com>
2015-12-28 21:50:15 +0600

    All Library

---
+++ ApplicationCustomConfig/ApplicationCustomConfig/Exceptions/ConfigurationCorruptedException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.Configuration.Exceptions
+{
+    /// <summary>
+    /// Выкидывается если конфигурационный файл поврежден или ключ "config" не найден
+    /// </summary>
+    [System.Serializable]
+    public class ConfigurationCorruptedException : Exception
+    {
+        /// <summary>
+        /// Выкидывается если конфигурационный файл поврежден или ключ "config" не найден
+        /// </summary>
+        public ConfigurationCorruptedException() { }
+        /// <summary>
+        /// Выкидывается если конфигурационный файл поврежден или ключ "config" не найден
+        /// </summary>
+        /// <param name="message">Сообщение исключения</param>
+        public ConfigurationCorruptedException(string message) : base(message) { }
+        /// <summary>
+        /// Выкидывается если конфигурационный файл поврежден или ключ "config" не найден
+        /// </summary>
+        /// <param name="message">Сообщение исключения</param>
+        /// <param name="inner">Входящее исключение</param>
+        public ConfigurationCorruptedException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ ApplicationCustomConfig/ApplicationCustomConfig/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Custom Configuration Library")]
+[assembly: AssemblyDescription("Adds Custom Configuration Management")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("craftersmine")]
+[assembly: AssemblyProduct("Custom Configuration Library")]
+[assembly: AssemblyCopyright("Copyright © craftersmine 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("af11c59d-9354-41ae-8de2-0ea90f633791")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
